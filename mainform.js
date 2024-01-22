5a63fd280e51c16d3d84ea9c2a181aafc0ad3969
jysowoqj <jysowoqj@naver.com>
2024-01-22 21:33:38 +0900

    주석 추가와 서버 복사 안내문 변경

--- mainform.js
+++ mainform.js
@@ -24,14 +24,14 @@ window.addEventListener('scroll', function() {
 
 // DISCORD BUTTON
 discordbt.addEventListener("click", function() {
-    const discord = "https://discord.gg/cwdWsDbere";
+    const discord = "https://discord.gg/cwdWsDbere"; // 추후 디스코드 서버 링크 바뀌면 변경 가능
     console.log("Discord Button Click")
     window.open(discord, "_blank");
 });
 
 // KAKAOTALK BUTTON
 kakaotalkbt.addEventListener("click", function() {
-    const kakaotalk = "https://open.kakao.com/o/gC2pyXNf";
+    const kakaotalk = "https://open.kakao.com/o/gC2pyXNf"; // 추후 카카오톡 서버 링크 바뀌면 변경 가능
     console.log("Kakaotalk Button Click")
     window.open(kakaotalk, "_blank")
 });
@@ -45,17 +45,18 @@ serveraddress.addEventListener("click", function() {
         console.log("Copy address >> " + address);
         alert("📑서버주소가 복사되었습니다!");
         })
-        .catch((error) => {
+        .catch((error) => { // 알 수 없는 오류가 발생했을때
         console.error("오류 발생:", error);
         });
     } else {
         console.log("해당 웹브라우저에서 클립보드에 접근할 수 없습니다.");
         alert("🚫 해당 웹브라우저에서 클립보드에 접근할 수 없습니다.");
-        alert("🚫 이 주소을 복사해주세요. >> zepick.kro.kr");
+        alert("🚫 이 주소을 복사해주세요.");
+        alert("서버 주소 : zepick.kro.kr") // 추후 서버 주소 바뀌면 바꿔야함
     }
 });
 
-// HOME BUTTON
+// HOME BUTTON : 클릭시 메인 페이지로 넘어감
 homebt.addEventListener("click", function() {
     home.style.display = 'block';
     rules.style.display = 'none';
@@ -63,7 +64,7 @@ homebt.addEventListener("click", function() {
     homebt.style.color = 'black';
 });
 
-// RULES BUTTON
+// RULES BUTTON : 클릭시 규칙 페이지로 넘어감
 rulesbt.addEventListener("click", function() {
     home.style.display = 'none';
     rules.style.display = 'block';
